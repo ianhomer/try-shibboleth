b0e31fe7eaf17fc7e610e97d5da1a2bcb1cb9802
Ian Homer <ihomer@worth.systems>
2018-11-01 17:02:56 +0000

    Property place holder with files

--- apache/scripts/docker-container-cmd.sh
+++ apache/scripts/docker-container-cmd.sh
@@ -10,29 +10,30 @@ readCertificateFile() {
 }
 
 #
-# Replace place holders in the file with the given place holders
+# Replace place holders in the file with the given place holder value, either from environment variable or load from
+# configuration file.
 #
 replacePlaceholders() {
   filename=$1
   shift
   for name in "$@" ; do
-    echo "$filename : placeholder replace - $name  "
+    echo "$filename : placeholder replace - $name:"
     value=`eval echo \\$$name`
     if [ -z "$value" ] ; then
-      filename_name="${name}_FILE"
-      filename=`eval echo \\$$filename_name`
-      echo "filename $filename"
-      if [ -z $filename ] ; then
+      var_filename_name="${name}_FILE"
+      var_filename=`eval echo \\$$var_filename_name`
+      echo "filename $var_filename"
+      if [ -z $var_filename ] ; then
         value="please-set-$name"
       else
         #
         # If environmental variable not provided and we have a file name defined, then read the file into this
         # variable.
         #
-        if [ -f $filename ] ; then
-          value=$(readCertificateFile $filename)
+        if [ -f $var_filename ] ; then
+          value=$(readCertificateFile $var_filename)
         else
-          value="please set ${name} or provide ${filename}"
+          value="please set ${name} or provide ${var_filename}"
         fi
       fi
     fi
@@ -49,10 +50,9 @@ replacePlaceholders() {
 : "${SP_CERTIFICATE_FILE:=/config/shibboleth/sp.cert}"
 : "${SP_CERTIFICATE_SUBJECT_NAME:=CN=localhost,O=organisation,L=location,ST=state,C=GB}"
 
-
-replacePlaceholders /etc/shibboleth/metadata/sp.xml SP_DOMAIN SP_CERTIFICATE_SUBJECT_NAME SP_CERTIFICATE
 replacePlaceholders /etc/shibboleth/shibboleth2.xml IDP_DOMAIN
 replacePlaceholders /etc/shibboleth/metadata/idp.xml IDP_DOMAIN IDP_SIGNING_CERTIFICATE IDP_ENCRYPTION_CERTIFICATE
+replacePlaceholders /etc/shibboleth/metadata/sp.xml SP_DOMAIN SP_CERTIFICATE_SUBJECT_NAME SP_CERTIFICATE XX
 
 /usr/sbin/shibd -f -c /etc/shibboleth/shibboleth2.xml -p /var/run/shibboleth/shibd.pid -w 30
 httpd-foreground
