52a03deddeacf1ebc95d9ee2bb898289c5c7e433
Ian Homer <ihomer@worth.systems>
2018-11-02 17:09:43 +0000

    Tweak how remote user is passed through from Apache

--- apache/config/shibboleth/README.md
+++ apache/config/shibboleth/README.md
@@ -1,7 +1,9 @@
 # Usage
 
-Example configuration files are located here.  If you wish to provide localised, secret ones then copy this
-folder to ~/.config/shibboleth and set the environment variable LOCAL_CONFIG_DIR to ~/.config
+These "secrets" are example keys and certificates and are safe to share in that they were generated for the
+sole purpose of demonstration in this example.   If you wish to provide localised, secret ones then copy this
+folder to apache/build/config/shibboleth and set the environment variable LOCAL_CONFIG_DIR to ~/.config.  Then redeploy
+docker containers.
 
 Note that the example certificates in this folder are entirely dummy certificates.
 
--- sp/src/main/java/com/purplepip/dojo/shibboleth/HomeController.java
+++ sp/src/main/java/com/purplepip/dojo/shibboleth/HomeController.java
@@ -5,11 +5,13 @@ import java.util.Collections;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
   @RequestMapping("/")
   public String index(HttpServletRequest request, Model model) {
@@ -18,6 +20,7 @@ public class HomeController {
         Collections.list(request.getHeaderNames())
             .stream()
             .collect(Collectors.toMap(Function.identity(), request::getHeader)));
+    LOG.info("Remote User = {}", request.getRemoteUser());
     model.addAttribute("remoteUser", request.getRemoteUser());
     model.addAttribute("now", LocalDate.now());
     return "index";
