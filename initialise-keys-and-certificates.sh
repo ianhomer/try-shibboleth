2d4abff203e0ae8c35ba44c445170d549a4c5146
Ian Homer <ihomer@worth.systems>
2018-11-16 16:00:21 +0000

    Tweak process for creating certificates

--- initialise-keys-and-certificates.sh
+++ initialise-keys-and-certificates.sh
@@ -11,31 +11,36 @@ mkdir -p $APACHE_SECRETS_DIR
 export APACHE_CERTS_DIR=apache/build/config/apache/certs
 mkdir -p $APACHE_CERTS_DIR
 
+createKeyAndCertificate() {
+  certificate=$1/certs/$2.crt
+  if [ ! -f ${certificate} ] ; then
+    properties=$1/certs/$2.properties
+    echo "creating   : ${certificate}"
+    echo > ${properties} << EOF
+distinguished_name=cn=localhost
+countryName=en
+EOF
+
+    mkdir -p $1/keys
+    mkdir -p $1/certs
+    openssl genrsa -out $1/keys/$2.key 2048
+    openssl req -new -x509 -batch -config ${properties} -key $1/keys/$2.key -out ${certificate} && \
+      echo "... created : ${certificate}"
+  else
+    echo "exists  : ${certificate}"
+  fi
+}
+
 # IDP SAML certificates
 # See https://wiki.shibboleth.net/confluence/display/CONCEPT/SAMLKeysAndCertificates for more details
 
-if [ ! -f $SHIBBOLETH_CERTS_DIR/idp-test-signing.crt ] ; then
-  openssl genrsa -out $SHIBBOLETH_SECRETS_DIR/idp-test-signing.key 2048
-  openssl req -new -x509 -key $SHIBBOLETH_SECRETS_DIR/idp-test-signing.key -out $SHIBBOLETH_CERTS_DIR/idp-test-signing.crt
-fi
-
-if [ ! -f $SHIBBOLETH_CERTS_DIR/idp-test-encryption.crt ] ; then
-  openssl genrsa -out $SHIBBOLETH_SECRETS_DIR/idp-test-encryption.key 2048
-  openssl req -new -x509 -key $SHIBBOLETH_SECRETS_DIR/idp-test-encryption.key -out $SHIBBOLETH_CERTS_DIR/idp-test-encryption.crt
-fi
+createKeyAndCertificate "apache/build/config/shibboleth" "idp-test-signing"
+createKeyAndCertificate "apache/build/config/shibboleth" "idp-test-encryption"
 
 # SP SAML certificates
 
-if [ ! -f $SHIBBOLETH_CERTS_DIR/sp-test.crt ] ; then
-  openssl genrsa -out $SHIBBOLETH_SECRETS_DIR/sp-test.key 2048
-  openssl req -new -x509 -key $SHIBBOLETH_SECRETS_DIR/sp-test.key -out $SHIBBOLETH_CERTS_DIR/sp-test.crt
-fi
-
-
-if [ ! -f $SHIBBOLETH_CERTS_DIR/sp-test-2.crt ] ; then
-  openssl genrsa -out $SHIBBOLETH_SECRETS_DIR/sp-test-2.key 2048
-  openssl req -new -x509 -key $SHIBBOLETH_SECRETS_DIR/sp-test-2.key -out $SHIBBOLETH_CERTS_DIR/sp-test-2.crt
-fi
+createKeyAndCertificate "apache/build/config/shibboleth" "sp-test"
+createKeyAndCertificate "apache/build/config/shibboleth" "sp-test-2"
 
 # Apache proxy certificates
 
